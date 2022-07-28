36d1fecc9b173bfce2f504805e616c0a786e97d3
Brayancorrea05 <77400637+Brayancorrea05@users.noreply.github.com>
2022-07-28 10:48:19 -0500

    nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,47 @@
 <template>
   <div class="h-screen">
-    <div class="p-2 px-14 pt-8 fixed z-50 w-screen " >
+    <div class="p-2 px-14 pt-8 fixed z-50 w-screen">
       <aside class="h-full">
-        <div class="flex w-full h-full p-5 justify-between bg-gradient-to-r from-[#1C2365] to-[#163B85] rounded-lg">
-          <div>
+        <div class="flex w-full h-full p-5 justify-between">
+          <div class="flex items-center">
             <img src="/Nav/CoexLogo.svg" alt="" />
           </div>
           <div class="flex items-center">
-            <div>
-              <app-btn class="bg-gradient-to-r from-red-500 to-red-400 p-1 text-white" @metodoBoton="serTalento">Ser
-                talento Coex</app-btn>
-            </div>
             <div class="flex">
-
-
-
               <div v-for="(item, i) in navItems" :key="i">
-
-                <NuxtLink :to="item.path" class="
-                    flex
-                    items-center
-                    p-2
-                    text-base
-                    font-medium
-                    text-white
-                  ">
-                  <span class="mx-6 " @click="show = !show" >{{ item.name }}</span>
+                <NuxtLink
+                  :to="item.path"
+                  class="flex items-center p-2 text-base font-medium text-white"
+                >
+                  <span
+                    class="
+                      mx-6
+                      hover:text-transparent
+                      bg-clip-text bg-gradient-to-r
+                      from-red-500
+                      to-red-400
+                    "
+                    >{{ item.name }}</span
+                  >
                 </NuxtLink>
-
               </div>
-
+              <div>
+                <app-btn
+                  class="
+                    bg-gradient-to-r
+                    from-red-500
+                    to-red-400
+                    p-1
+                    mx-6
+                    text-white
+                    hover:bg-gradient-to-r
+                    from-red-300
+                    to-red-500
+                  "
+                  @metodoBoton="serTalento"
+                  >Ser talento Coex</app-btn
+                >
+              </div>
             </div>
             <div class="px-4">
               <nuxt-link to=""><img src="/Nav/user.svg" alt="" /></nuxt-link>
@@ -39,16 +51,8 @@
       </aside>
     </div>
     <div class="">
-
-      <transition name="fade">
-        <p v-if="show">
-          <Nuxt />
-        </p>
-      </transition>
+      <Nuxt />
     </div>
-
-
-
   </div>
 </template>
 
@@ -57,12 +61,13 @@ export default {
   name: 'NavbarDefault',
   data() {
     return {
-
-      show: true,
-
       navItems: [
         {
           name: 'Inicio',
+          path: '',
+        },
+        {
+          name: '¿Quienes somos?',
           path: '',
         },
         {
@@ -82,33 +87,16 @@ export default {
           path: '',
         },
       ],
-
     }
   },
   methods: {
     serTalento() {
-      this.show = !this.show
       this.$router.push('/ser-talento')
-
-    }
+    },
   },
 }
 </script>
 
 
 <style scoped>
-
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity .4s
-}
-
-.fade-enter,
-.fade-leave-to
-
-/* .fade-leave-active below version 2.1.8 */
-  {
-  opacity: 0
-}
 </style>
