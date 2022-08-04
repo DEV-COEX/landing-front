fd3ac339c346caea6873de6e978b33a29cef19a2
JuanJose9911 <juanjosemartinez1999@outlook.com>
2022-08-04 09:31:03 -0500

    Integracion

--- components/AppCardOferta.vue
+++ components/AppCardOferta.vue
@@ -4,19 +4,23 @@
             <p class="text-xl leading-normal font-bold  text-transparent bg-clip-text bg-gradient-to-r
                       from-[#FFDF8D]
                       via-[#FF9838]
-                      to-[#dab255]">¿sexo?</p>
+                      to-[#dab255]">{{ offer.tittle }}</p>
         </div>
         <div class="">
-            <p class="text-xl  text-white py-3 ">sexo sexo sexo sexo sexo sexo sexo sexo sexo  sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo sexo
-            </p>
-
+            <p class="text-xl  text-white py-3 ">{{ offer.description }}</p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "CardOferta"
+    name: "CardOferta",
+    props: {
+        offer: {
+            type: Object,
+            required: true
+        }
+    },
 }
 </script>
 
--- pages/contrata-talento/index.vue
+++ pages/contrata-talento/index.vue
@@ -7,20 +7,16 @@
           <p class="text-5xl leading-normal font-bold  w-4/5 text-center text-transparent bg-clip-text bg-gradient-to-r
                       from-[#FFDF8D]
                       via-[#FF9838]
-                      to-[#dab255]">¿Necesitas talento T.I para tu empresa?</p>
+                      to-[#dab255]">{{ pageText?.tittle }}</p>
         </div>
         <div class="flex justify-center">
-          <p class="text-3xl w-2/5 text-center text-white py-10">Nuestra metodología está orientada a satisfacer el
-            mercado laboral de programadores, mediante las siguientes dos modalidades de oferta:
-
-          </p>
-
+          <p class="text-3xl w-2/5 text-center text-white py-10">{{ pageText?.description }}</p>
         </div>
         <div class="grid justify-center items-center">
           <div class="p-7">
-            <app-card-oferta />
+            <app-card-oferta  v-for="offer in offers" :key="offer.id"  :offer="offer"/>
           </div>
-         
+
         </div>
       </div>
     </div>
@@ -32,18 +28,25 @@ export default {
   name: "ContrataTalento",
   data() {
     return {
-      pageText: {}
+      pageText: {},
+      offers: []
     }
   },
   async mounted() {
+    await this.getOffers();
     this.getPageText()
     await this.$store.dispatch("fetchTexts");
     this.getPageText()
+
   },
   methods: {
     getPageText() {
       this.$store.commit("filterText", "contratar-talento")
-      this.pageText = this.$store.state.text
+      this.pageText = this.$store.state.text[0]
+    },
+    async getOffers() {
+      const response = await this.$axios.get("offers");
+      this.offers = response.data;
     }
   }
 }
