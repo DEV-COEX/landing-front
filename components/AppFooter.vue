90a54e2ec7e117f88f8154eeb3d25b1f5774fe12
UpHeroK <66838653+UpHeroK@users.noreply.github.com>
2022-08-08 18:30:27 -0500

    sexo blur

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="h-auto bg-[#171929] text-white z-10 relative">
-    <div class="grid grid-cols-3 justify-between">
-      <div class="flex flex-col p-20 pb-10">
+  <div class="h-screen flex flex-col justify-end lg:block lg:h-auto bg-[#171929] text-white z-10 relative text-sm lg:text-base">
+    <div class="flex flex-col justify-between lg:grid lg:grid-cols-3 lg:justify-between">
+      <div class="flex items-center lg:items-start flex-col lg:p-20 lg:pb-10 px-5 py-8">
         <img
-          class="w-[18rem] pb-5"
+          class="lg:w-[18rem] w-[10rem] pb-5"
           src="/footer/coexGroup.svg"
           alt="CoexLogo"
         />
-        <div>
+        <div class="text-justify ">
           Es un Bootcamp en el cual formamos la base del hacer, del ser y el
           saber de cada talento juvenil que quiere ser Talento, provenientes de
           diversos tipos de comunidades de nuestra ciudad, bajo un ambiente de
           alta productividad dónde se entrenan como desarrolladores de software.
         </div>
       </div>
-      <div class="flex flex-col items-center justify-center">
-        <div v-for="(item, i) in navitems" :key="i" class="mt-1 p-2">
-          <NuxtLink :to="item.path" class="font-medium text-white">
+      <div class="lg:flex lg:flex-col lg:items-center justify-between lg:justify-center grid grid-cols-2 p-5 lg:p-0 ">
+        <div v-for="(item, i) in navitems" :key="i" class="lg:mt-1 mt-4">
+          <NuxtLink :to="item.path" class="font-medium  text-white">
             <span
               class="
-                mx-6
+                lg:mx-6
+                lg:font-bold
+                font-normal
                 hover:text-transparent
                 bg-clip-text bg-gradient-to-r
                 from-red-500
@@ -35,8 +37,9 @@
           </NuxtLink>
         </div>
       </div>
-      <div class="flex flex-col justify-center justify bg-center w-4/5">
-        <div class="text-2xl text-center pb-11">SÍGUENOS</div>
+      <div class="w-full flex justify-center">
+      <div class="flex flex-col justify-center w-[70%] pb-5 lg:pb-0 lg:w-4/5">
+        <div class="text-2xl text-center pb-3 lg:pb-11">SÍGUENOS</div>
         <div class="flex w-full justify-evenly">
           <app-btn>
             <img src="/footer/instLogo.svg" alt="InstagramLogo" /></app-btn
@@ -45,12 +48,12 @@
             ><img src="/footer/Facebook.svg" alt="FacebookLogo" /></app-btn
           ><app-btn><img src="/footer/twLogo.svg" alt="TwitterLogo" /></app-btn>
         </div>
-      </div>
+      </div></div>
     </div>
-    <div class="border-t-[1px] border-white mx-20 text-white pb-6">
-      <div class="flex flex-col w-[55%]">
-        <span>Coex</span>
-        <div class="flex justify-between">
+    <div class="border-t-[1px] border-white lg:mx-20 flex text-white pb-6">
+      <div class="flex flex-col w-full lg:w-[55%]">
+        <span class="hidden lg:block">Coex</span>
+        <div class="flex lg:flex-row flex-col mt-6 lg:mt-0 items-center justify-between">
           <div>Derechos reservados.</div>
           <div>Desarrollado por COEX</div>
         </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,124 @@
 <template>
-  <div class="h-screen w-full bg-gradient-to-r from-[#321586] via-[#4736df] to-[#2E1477]">
-    <div class="p-2 px-14 pt-8 fixed z-50 w-full hidden lg:block ">
-      <aside class="h-full w-full ">
-        <div class="flex w-full h-full p-5 justify-between">
+  <div
+    class="
+      min-h-screen
+      w-full
+      bg-gradient-to-r
+      from-[#321586]
+      via-[#4736df]
+      to-[#2E1477]
+    "
+  >
+    <!-- navbar mobile -->
+    <div
+      v-show="show"
+      class="
+        h-screen
+        w-full
+        z-30
+        bg-[#1719298c]
+        fixed
+        top-0
+        bottom-0
+        overflow-hidden
+        lg:hidden
+      "
+    >
+      <div class="flex h-full w-full flex-col items-center justify-center z-50">
+        <div class="w-full h-auto text-white text-center" @click="show = !show">Sexo Cerrador de navbars 🥵 🥵 🥵 🥵 🥵  </div>
+        <div v-for="(item, i) in navItems" :key="i" @click="show = !show">
+          <NuxtLink
+            :to="item.path"
+            class="flex my-1 text-base font-normal text-white"
+          >
+            <span
+              class="
+                p-1
+                my-3
+                hover:text-transparent
+                bg-clip-text bg-gradient-to-r
+                from-red-500
+                to-red-400
+                transition
+                ease-in-out
+                delay-150
+                hover:-translate-y-1 hover:scale-110
+                duration-300
+              "
+              @click="item.method"
+            >
+              {{ item.name }}
+            </span>
+          </NuxtLink>
+        </div>
+        <div @click="show = !show">
+          <app-btn
+            class="
+              bg-gradient-to-r
+              from-red-500
+              to-red-400
+              p-[2px]
+              text-white
+              hover:from-red-400 hover:to-red-500
+            "
+            @click="serTalento"
+            >Ser talento Coex
+          </app-btn>
+        </div>
+      </div>
+    </div>
+    <div
+      :class="[show ? 'blur-sm' : 'blur-none']"
+      class="lg:p-2 lg:px-14 pt-8 fixed z-50 w-full"
+    >
+      <aside class="h-full w-full">
+        <div
+          class="
+            flex
+            w-full
+            h-full
+            px-5
+            Z-40
+            lg:p-5
+            justify-between
+            items-center
+          "
+        >
           <div class="flex items-center">
-            <img src="/Nav/CoexLogo.svg" alt=""/>
+            <img src="/Nav/CoexLogo.svg" class="w-[8rem]" alt="LogoCoex" />
           </div>
-          <div class="flex items-center ">
+          <div class="text-white lg:hidden" @click="show = !show">
+            <img src="/Nav/menu.svg" alt="desplegable" />
+          </div>
+          <!-- navbar mobile -->
+          <!-- items navbar -->
+          <div class="hidden lg:flex items-center">
             <div class="flex">
               <div v-for="(item, i) in navItems" :key="i">
-                <NuxtLink id="sexo" :to="item.path" class="flex items-center p-2 text-base font-medium text-white">
+                <NuxtLink
+                  :to="item.path"
+                  class="flex items-center p-2 text-base font-medium text-white"
+                >
                   <span
-                    class=" mx-6
+                    class="
+                      mx-6
                       hover:text-transparent
                       bg-clip-text bg-gradient-to-r
                       from-red-500
                       to-red-400
-                      transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300
-                    " @click="item.method">
+                      transition
+                      ease-in-out
+                      delay-150
+                      hover:-translate-y-1 hover:scale-110
+                      duration-300
+                    "
+                    @click="item.method"
+                  >
                     {{ item.name }}
                   </span>
                 </NuxtLink>
               </div>
-              <div class="w-56 ">
-
+              <div class="w-56">
                 <app-btn
                   class="
                     bg-gradient-to-r
@@ -33,22 +128,23 @@
                     mx-6
                     text-white
                     hover:from-red-400 hover:to-red-500
-
-
-                  " @click="serTalento">Ser talento Coex
+                  "
+                  @click="serTalento"
+                  >Ser talento Coex
                 </app-btn>
               </div>
             </div>
           </div>
+          <!-- fin items -->
         </div>
       </aside>
     </div>
     <div class="">
       <transition name="fade">
-        <Nuxt ref="home"/>
+        <Nuxt ref="home" :class="[show? 'blur-sm':'blur-none']"  />
       </transition>
     </div>
-    <app-footer :navitems="navItems" class="hidden lg:block"/>
+    <app-footer :class="[show? 'blur-sm':'blur-none']" :navitems="navItems" />
   </div>
 </template>
 
@@ -57,6 +153,7 @@ export default {
   name: 'NavbarDefault',
   data() {
     return {
+      show: false,
       navItems: [
         {
           name: 'Inicio',
@@ -64,46 +161,36 @@ export default {
           method: () => {
             if (this.$route.path === '/') {
               const estado = true
-              const estadoHome= false
-              this.$store.dispatch('home', estadoHome);
-              this.$store.dispatch('animation', estado);
+              const estadoHome = false
+              this.$store.dispatch('home', estadoHome)
+              this.$store.dispatch('animation', estado)
             }
-          }
+          },
         },
         {
           name: '¿Quienes somos?',
           path: '/about',
-          method: () => {
-
-          }
+          method: () => {},
         },
         {
           name: 'Fábrica de software',
           path: '/fabrica',
-          method: () => {
-
-          }
+          method: () => {},
         },
         {
           name: 'Contrata talentos',
           path: '/contrata-talento',
-          method: () => {
-
-          }
+          method: () => {},
         },
         {
           name: 'Educación',
           path: '/educacion',
-          method: () => {
-
-          }
+          method: () => {},
         },
         {
           name: 'Fundación',
           path: '/fundacion',
-          method: () => {
-
-          }
+          method: () => {},
         },
       ],
     }
@@ -113,8 +200,6 @@ export default {
       this.$router.push('/ser-talento')
     },
   },
-
-
 }
 </script>
 
@@ -123,13 +208,11 @@ export default {
 .fade-enter-active,
 .fade-leave-active {
   transition: 3s;
-
 }
 
 .fade-enter .fade-leave-to
 
-  /* .fade-leave-active below version 2.1.8 */
-{
+  /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
 </style>
