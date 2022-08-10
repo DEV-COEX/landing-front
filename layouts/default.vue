78c71eb4fb3b4b1afafef752cab46b14e091ab10
UpHeroK <66838653+UpHeroK@users.noreply.github.com>
2022-08-10 10:17:50 -0500

    altaneria pa mi gente

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="
+  <div
+    class="
       min-h-screen
       w-full
       bg-gradient-to-r
       from-[#321586]
       via-[#4736df]
       to-[#2E1477]
-    ">
+    "
+  >
     <!-- navbar mobile -->
-    <div v-show="show" class="
+    <div
+      v-show="show"
+      class="
         h-screen
         w-full
         z-30
@@ -18,7 +22,8 @@
         bottom-0
         overflow-hidden
         lg:hidden
-      ">
+      "
+    >
       <div class="flex h-full w-full flex-col items-center justify-center z-50">
         <div class="w-full h-auto text-white text-center" @click="ChangeShow">
           Sexo Cerrador de navbars 🥵 🥵 🥵 🥵 🥵
@@ -41,7 +46,9 @@
                 delay-150
                 hover:-translate-y-1 hover:scale-110
                 duration-300
-              " @click="item.method">
+              "
+              @click="item.method"
+            >
               {{ item.name }}
             </span>
           </NuxtLink>
@@ -55,17 +62,21 @@
               p-[2px]
               text-white
               hover:from-red-400 hover:to-red-500
-            " @click="serTalento">Ser talento Coex
+            "
+            @click="serTalento"
+            >Ser talento Coex
           </app-btn>
         </div>
       </div>
     </div>
     <div
       :class="[show ? 'blur-sm' : '']"
-      class="lg:p-2 lg:px-14 pt-8 fixed z-50 w-full bg-clip-padding " style="backdrop-filter: blur(10px);"
+      class="lg:p-2 lg:px-14 pt-8 fixed z-50 w-full bg-clip-padding"
+      style="backdrop-filter: blur(10px)"
     >
       <aside class="h-full w-full">
-        <div class="
+        <div
+          class="
             flex
             w-full
             h-full
@@ -74,7 +85,8 @@
             lg:p-5
             justify-between
             items-center
-          ">
+          "
+        >
           <div class="flex items-center">
             <img src="/Nav/CoexLogo.svg" class="w-[8rem]" alt="LogoCoex" />
           </div>
@@ -86,8 +98,12 @@
           <div class="hidden lg:flex items-center">
             <div class="flex">
               <div v-for="(item, i) in navItems" :key="i">
-                <NuxtLink :to="item.path" class="flex items-center p-2 text-base font-medium text-white">
-                  <span class="
+                <NuxtLink
+                  :to="item.path"
+                  class="flex items-center p-2 text-base font-medium text-white"
+                >
+                  <span
+                    class="
                       mx-6
                       hover:text-transparent
                       bg-clip-text bg-gradient-to-r
@@ -98,13 +114,16 @@
                       delay-150
                       hover:-translate-y-1 hover:scale-110
                       duration-300
-                    " @click="item.method">
+                    "
+                    @click="item.method"
+                  >
                     {{ item.name }}
                   </span>
                 </NuxtLink>
               </div>
               <div class="w-56">
-                <app-btn class="
+                <app-btn
+                  class="
                     bg-gradient-to-r
                     from-red-500
                     to-red-400
@@ -112,7 +131,9 @@
                     mx-6
                     text-white
                     hover:from-red-400 hover:to-red-500
-                  " @click="serTalento">Ser talento Coex
+                  "
+                  @click="serTalento"
+                  >Ser talento Coex
                 </app-btn>
               </div>
             </div>
@@ -126,7 +147,11 @@
         <Nuxt ref="home" :class="[show ? 'blur-sm' : '']" class="lg:blur-0" />
       </transition>
     </div>
-    <app-footer :class="[show ? 'blur-sm' : '']" class="lg:blur-0" :navitems="navItems" />
+    <app-footer
+      :class="[show ? 'blur-sm' : '']"
+      class="lg:blur-0"
+      :navitems="navItems"
+    />
   </div>
 </template>
 
@@ -135,6 +160,7 @@ export default {
   name: 'NavbarDefault',
   data() {
     return {
+      windowWidth: null,
       show: false,
       navItems: [
         {
@@ -156,42 +182,85 @@ export default {
         {
           name: '¿Quienes somos?',
           path: '/about',
+<<<<<<< Updated upstream
           method: () => {
             window.scrollY(0)
           },
+=======
+          method: () => {},
+>>>>>>> Stashed changes
         },
         {
           name: 'Fábrica de software',
           path: '/fabrica',
+<<<<<<< Updated upstream
           method: () => {
             window.scrollY(0)
           },
+=======
+          method: () => {},
+>>>>>>> Stashed changes
         },
         {
           name: 'Contrata talentos',
           path: '/contrata-talento',
+<<<<<<< Updated upstream
           method: () => {
             window.scrollY(0)
           },
+=======
+          method: () => {},
+>>>>>>> Stashed changes
         },
         {
           name: 'Educación',
           path: '/educacion',
+<<<<<<< Updated upstream
           method: () => {
             window.scrollY(0)
           },
+=======
+          method: () => {},
+>>>>>>> Stashed changes
         },
         {
           name: 'Fundación',
           path: '/fundacion',
+<<<<<<< Updated upstream
           method: () => {
             window.scrollY(0)
           },
+=======
+          method: () => {},
+>>>>>>> Stashed changes
         },
       ],
     }
   },
+  mounted() {
+    this.$nextTick(() => {
+    /* eslint-disable-next-line */
+      if (process.client) {
+        this.windowWidth = window.innerWidth
+        window.addEventListener('resize', this.onResize)
+      }
+    })
+  },
+  beforeDestroy() {
+    /* eslint-disable-next-line */
+    if (process.client) {
+      window.removeEventListener('resize', this.onResize)
+    }
+  },
   methods: {
+    onResize() {
+      if (process.client) {
+        this.windowWidth = window.innerWidth
+        if (this.windowWidth > 1024 && this.show) {
+          this.ChangeShow()
+        }
+      }
+    },
     serTalento() {
       this.$router.push('/ser-talento')
     },
@@ -216,8 +285,7 @@ export default {
 
 .fade-enter .fade-leave-to
 
-/* .fade-leave-active below version 2.1.8 */
-  {
+/* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
 </style>
