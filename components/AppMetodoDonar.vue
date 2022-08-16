c224b1971706b16b489cb5d4d4b5326e821e1c7f
JuanJose9911 <juanjosemartinez1999@outlook.com>
2022-08-16 09:03:33 -0500

    Update AppMetodoDonar.vue

--- components/AppMetodoDonar.vue
+++ components/AppMetodoDonar.vue
@@ -1,59 +1,65 @@
 <template>
-    <div class="m-2">
-        <label class="block text-transparent bg-clip-text bg-gradient-to-r
+  <div class="m-2">
+    <label class="block text-transparent bg-clip-text bg-gradient-to-r
                       from-[#A6CBFF]
                       to-[#C8DEFF] text-base font-medium mb-2" :for='label'>{{ label }}</label>
+    <div>
+      <div class="flex items-center justify-between">
         <div>
-            <div class="flex items-center justify-between">
-                <div>
-                    <app-btn>
-                        <img src="/Metodos/paypal.svg" />
-                    </app-btn>
-                </div>
-                <div class="flex ">
-                    <div v-for="(item, i) in metodos" :key="i">
-                        <app-btn class="m-2 "> <img class="h-10"  :src=item.image />
-                        </app-btn>
-                    </div>
-                </div>
-
-            </div>
-            <div class="flex justify-end">
-                <label>seleccione uno de estos metodos</label>
-            </div>
-
+          <app-btn type="button">
+            <img :src="selected"/>
+          </app-btn>
+        </div>
+        <div class="flex ">
+          <div v-for="(item, i) in metodos" :key="i">
+            <app-btn type="button" class="m-2" @click="changeImage(item)"><img class="h-10" :src="item.image"/>
+            </app-btn>
+          </div>
         </div>
 
+      </div>
+      <div class="flex justify-end">
+        <label>seleccione uno de estos metodos</label>
+      </div>
+
     </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-    name: "metodosDonar",
-    props: {
-        label: {
-            type: String,
-            default: null
-        },
+  name: "metodosDonar",
+  props: {
+    label: {
+      type: String,
+      default: null
     },
-    data() {
-        return {
-            metodos: [
-                {
-                    name: "paypal",
-                    image: "/Metodos/paypal.svg"
-                },
-                {
-                    name: "paypal",
-                    image: "/Metodos/visa.svg"
-                },
-                {
-                    name: "paypal",
-                    image: "/Metodos/mastercard.svg"
-                }
-            ]
+  },
+  data() {
+    return {
+      metodos: [
+        {
+          name: "paypal",
+          image: "/Metodos/paypal.svg"
+        },
+        {
+          name: "paypal",
+          image: "/Metodos/visa.svg"
+        },
+        {
+          name: "paypal",
+          image: "/Metodos/mastercard.svg"
         }
+      ],
+      selected: "/Metodos/paypal.svg"
     }
+  },
+  methods: {
+    changeImage(item) {
+      this.selected = item.image;
+    }
+  }
 }
 </script>
 
