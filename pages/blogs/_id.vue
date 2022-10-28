af2a10a764b897328726b6805a9e76e2e2cbd531
Mishell  Merchán <mishell2172329@correo.uis.edu.co>
2022-10-28 12:29:50 -0500

    Integration blogs

--- pages/blogs/_id.vue
+++ pages/blogs/_id.vue
@@ -2,7 +2,8 @@
   <div class="bg-[#1C233A] flex justify-center">
     <div class="w-3/5 h-auto mt-6 md:px-4">
       <h1
-        class="lg:text-5xl md:text-3xl lg:leading-relaxed font-bold text-2xl text-center text-transparent bg-clip-text bg-gradient-to-r from-[#FFDF8D] via-[#FF9838] to-[#dab255]">
+        class="lg:text-5xl md:text-3xl lg:leading-relaxed font-bold text-2xl text-center text-transparent bg-clip-text bg-gradient-to-r from-[#FFDF8D] via-[#FF9838] to-[#dab255]"
+      >
         {{ blog.title }}
       </h1>
       <div class="flex text-white place-content-between mt-4">
@@ -18,80 +19,100 @@
           <div>Compartir</div>
         </div>
       </div>
-      <div class=" mt-4 ">
-        <img class="object-cover bg-fixed h-96 w-full" :src="blog.image?.url">
+      <div class="mt-4">
+        <img class="object-cover bg-fixed h-96 w-full" :src="blog.image?.url" />
       </div>
       <div class="flex text-white space-x-4 items-center mt-4">
         <div>
-          <img class="object-cover bg-fixed max-h-14 w-full" :src="blog.autor_image?.url">
+          <img
+            class="object-cover bg-fixed max-h-14 w-full"
+            :src="blog.autor_image?.url"
+          />
         </div>
         <div>
           <h3>{{ blog.autor_name }}</h3>
         </div>
       </div>
 
-      <div class=" bg-white bg-opacity-10 mt-10 text-white list-none">
-        <h6 
-        class="
-        pl-12 md:text-3xl
-        lg:leading-relaxed 
-        font-bold
-        text-left text-transparent 
-        bg-clip-text bg-gradient-to-r 
-        from-[#FFDF8D] 
-        via-[#FF9838] 
-        to-[#dab255]">In this article </h6>
-        <li class="pl-12 py-4" v-for="(topic,index) in topic_blog" :key="topic.id">
-          {{index+1}}. {{topic}}
+      <div class="bg-white bg-opacity-10 mt-10 text-white list-none">
+        <h6
+          class="pl-12 md:text-3xl lg:leading-relaxed font-bold text-left text-transparent bg-clip-text bg-gradient-to-r from-[#FFDF8D] via-[#FF9838] to-[#dab255]"
+        >
+          In this article
+        </h6>
+        <li
+          class="pl-12 py-4"
+          v-for="(topic, index) in topic_blog"
+          :key="topic.id"
+        >
+          {{ index + 1 }}. {{ topic }}
         </li>
       </div>
 
-      <div class=" mt-4 font-bold text-white text-xl">
+      <div class="mt-4 font-bold text-white text-xl">
         <h1>{{ blog.introduction_blog }}</h1>
       </div>
-      <div class=" mt-4 pb-8 text-white">
+      <div class="mt-4 pb-8 text-white">
         <h1>{{ blog.content_blog }}</h1>
       </div>
       <div class="text-center justify-center">
-        <a class=" pl-12 md:text-3xl
-        lg:leading-relaxed 
-        font-bold
-        text-left text-transparent 
-        bg-clip-text bg-gradient-to-r 
-        from-[#FFDF8D] 
-        via-[#FF9838] 
-        to-[#dab255] 
-        " :href="''">Sigue leyendo </a>
+        <a
+          class="pl-12 md:text-3xl lg:leading-relaxed font-bold text-left text-transparent bg-clip-text bg-gradient-to-r from-[#FFDF8D] via-[#FF9838] to-[#dab255]"
+          :href="''"
+          >Sigue leyendo
+        </a>
         <p class="text-white w-3/4 m-auto">
-          Blogs are a great method to increase traffic and generate leads. Make a profit. Get compensated for your efforts.
+          Blogs are a great method to increase traffic and generate leads. Make
+          a profit. Get compensated for your efforts.
         </p>
-
       </div>
+
+      <div class="grid grid-cols-3 content-center my-8">
+        <div>
+          <h6
+            class="items-start border-b-2 border-[rgba(78, 146, 249, 0.2)] text-white"
+          >
+            Category
+          </h6>
+          <h3
+            class="lg:leading-relaxed font-medium text-left text-transparent bg-clip-text bg-gradient-to-r from-[#FFDF8D] via-[#FF9838] to-[#dab255]"
+          >
+          {{ blog.title }}
+          </h3>
+          <div class="grid grid-rows-3 grid-flow-col gap-0.5">
+            <div class="row-span-3 ...">
+              <img
+                class="object-cover bg-fixed h-4/6 w-3/4"
+                :src="blog.autor_image?.url"
+              />
+            </div>
+            <div class="col-span-2 content-center">
+              <h3
+                class="lg:leading-relaxed font-medium text-left text-transparent bg-clip-text bg-gradient-to-r from-[#87B8FF] via-[#4490F9] to-[#4490F9]"
+              >
+                {{ blog.autor_name }}
+              </h3>
+            </div>
+            <div class="row-span-2 col-span-2 text-white text-sm leading-4">
+              <div class="flex space-x-4">
+                <div>{{ blog.blog_created_date }}</div>
+                <div>{{ blog.time_reading }} min read</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="text-center">
         <app-btn
-        class="
-          bg-gradient-to-r
-          from-blue-500
-          to-blue-400
-          p-[2px]
-          text-white
-          hover:from-blue-400
-          hover:to-blue-500
-          mb-5
-        "
+          class="bg-gradient-to-r from-blue-500 to-blue-400 p-[2px] text-white hover:from-blue-400 hover:to-blue-500 mb-5"
         >
-        Ver todas las entradas
+          Ver todas las entradas
         </app-btn>
-
       </div>
-      
-      
-
     </div>
-
   </div>
 </template>
-
 
 <script>
 export default {
@@ -100,10 +121,10 @@ export default {
   data() {
     return {
       blogs: null,
-      blog:{},
+      blog: {},
       topic_blogs: null,
       topic_blog: null,
-      markdown:null,
+      markdown: null,
     }
   },
   async mounted() {
@@ -111,7 +132,7 @@ export default {
     await this.getBlog()
   },
   methods: {
-      // TRAERME TODOS LOS BLOGS
+    // TRAERME TODOS LOS BLOGS
     // async getBlogs() {
     //   const { data } = await this.$axios.get('Blogs')
     //   this.blogs = data
@@ -124,16 +145,14 @@ export default {
     //   })
     // },
 
-      // TRAERME UN BLOG
+    // TRAERME UN BLOG
     async getBlog() {
       const { data } = await this.$axios.get('Blogs/1')
-        this.blog = data
-        this.blog.image.url = `https://api.cms.coex.com.co${this.blog.image.formats.large.url}`
-        this.blog.autor_image.url = `https://api.cms.coex.com.co${this.blog.autor_image.url}`
-        this.topic_blog = this.blog.relevant_topic_blog.split(',')         
-      },
-    
-
+      this.blog = data
+      this.blog.image.url = `https://api.cms.coex.com.co${this.blog.image.formats.large.url}`
+      this.blog.autor_image.url = `https://api.cms.coex.com.co${this.blog.autor_image.url}`
+      this.topic_blog = this.blog.relevant_topic_blog.split(',')
+    },
   },
 }
 </script>
